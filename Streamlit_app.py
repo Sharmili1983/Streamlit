8bd7dc3a9bd4fd7fa82ee97a2c7b919e734b3f8b
Sharmili1983 <119340983+Sharmili1983@users.noreply.github.com>
2023-08-31 19:40:56 +0530

    Add files via upload

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,3 +5,37 @@ Type "help", "copyright", "credits" or "license()" for more information.
 ... import pandas as pd
 ... from sklearn import datasets
 ... from sklearn.ensemble import RandomForestClassifier
+
+st.write("""
+#Find the Greatest number
+
+This app predicts the greatest nmber out of three numbers.
+""")
+#Get Input
+
+st.header('User Input Parameters')
+
+def user_input_features():
+  No1 = st.number_input("Number1: ")
+  No2 = st.number_input("Number2: ")
+  No3 = st.number_input("Number3: ")
+
+  if (No1>No2 and No1>No3):
+    print("Greatest number is ", No1)
+  elif (No2>No1 and No2>No3):
+    print("Greatest number is ", No2)
+  elif (No3>No1 and No3>No2):
+    print("Greatest number is ", No3)
+
+  data = {'Number1': No1,
+          'Number2': No2,
+          'Number3': No3
+          }
+
+  features = pd.DataFrame(data, index=[0])
+  return features
+
+data = user_input_features()
+
+st.subheader('User Input parameters')
+st.write(data.to_dict())
