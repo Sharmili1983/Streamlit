ed9f47d17a359d1df58b133d733ded1b487b8aff
Sharmili1983 <119340983+Sharmili1983@users.noreply.github.com>
2023-08-31 19:35:29 +0530

    Add files via upload

---
+++ Streamlit_app.py
@@ -0,0 +1,7 @@
+Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> !pip install -q streamlit
+... import streamlit as st
+... import pandas as pd
+... from sklearn import datasets
+... from sklearn.ensemble import RandomForestClassifier
