6da1ac2c5879296bb7854b655f5026df40443221
Sharmili1983 <119340983+Sharmili1983@users.noreply.github.com>
2023-08-31 19:58:41 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,8 +1,6 @@
 #pip install -q streamlit
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
 #Find the Greatest number
