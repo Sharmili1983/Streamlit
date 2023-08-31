01e213fcacc7c224f8f27402b782dab57a742393
Sharmili1983 <119340983+Sharmili1983@users.noreply.github.com>
2023-08-31 19:55:53 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,19 +1,16 @@
-Python 3.11.4 (tags/v3.11.4:d2340ef, Jun  7 2023, 05:45:37) [MSC v.1934 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> !pip install -q streamlit
-... import streamlit as st
-... import pandas as pd
-... from sklearn import datasets
-... from sklearn.ensemble import RandomForestClassifier
+!pip install -q streamlit
+import streamlit as st
+import pandas as pd
+from sklearn import datasets
+from sklearn.ensemble import RandomForestClassifier
 
 st.write("""
 #Find the Greatest number
 
-This app predicts the greatest nmber out of three numbers.
+This app predicts the Greatest number out of three numbers.
 """)
-#Get Input
 
-st.header('User Input Parameters')
+st.header('User Input Parameters:')
 
 def user_input_features():
   No1 = st.number_input("Number1: ")
