830407dde21b20ced7fa622048c73d8478bb7ad7
Sharmili1983 <119340983+Sharmili1983@users.noreply.github.com>
2023-08-31 20:14:01 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -12,11 +12,11 @@ def user_input_features():
   No2 = st.number_input("Number2: ")
   No3 = st.number_input("Number3: ")
   if (No1>No2 and No1>No3):
-    st.print("Greatest number is ", No1)
+    st.write("Greatest number is ", No1)
   elif (No2>No1 and No2>No3):
-    st.print("Greatest number is ", No2)
+    st.write("Greatest number is ", No2)
   elif (No3>No1 and No3>No2):
-    st.print("Greatest number is ", No3)
+    st.write("Greatest number is ", No3)
 
   data = {'Number1': No1,
           'Number2': No2,
