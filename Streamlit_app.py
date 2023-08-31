52ac3022a5a839c60936ac3a40876107382fb904
Sharmili1983 <119340983+Sharmili1983@users.noreply.github.com>
2023-08-31 20:57:09 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,21 +1,22 @@
 import streamlit as st
 import pandas as pd
 
-st.title(":red[Find the Greatest number]")
+st.title(":blue[Find the Greatest number]")
 st.write("""This App predicts the Greatest number out of three numbers""")
 
 st.subheader('Enter any three numbers!')
+primary_color = "#00f900"
 
 def user_input_features():
   No1 = st.number_input("Number1: ")
   No2 = st.number_input("Number2: ")
   No3 = st.number_input("Number3: ")
   if (No1>No2 and No1>No3):
-    st.write("_:Yellow[Greatest number is ]_", No1)
+    st.write("_:primary_color[Greatest number is ]_", No1)
   elif (No2>No1 and No2>No3):
-     st.write("_:Yellow[Greatest number is ]_", No2)
+     st.write("_:primary_color[Greatest number is ]_", No2)
   elif (No3>No1 and No3>No2):
-     st.subheader("_:Yellow[Greatest number is ]_", No3)
+     st.write("_:primary_color[Greatest number is ]_", No3)
 
   data = {'Number1': No1,
           'Number2': No2,
