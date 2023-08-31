0585a0cdac850c8ecf204eb804a97a3c8fe03942
Sharmili1983 <119340983+Sharmili1983@users.noreply.github.com>
2023-08-31 20:12:10 +0530

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -5,18 +5,18 @@ import pandas as pd
 st.title("Find the Greatest number")
 st.write("""This App predicts the Greatest number out of three numbers""")
 
-st.header('User Input Parameters:')
+st.subheader('User Input Parameters:')
 
 def user_input_features():
   No1 = st.number_input("Number1: ")
   No2 = st.number_input("Number2: ")
   No3 = st.number_input("Number3: ")
   if (No1>No2 and No1>No3):
-    print("Greatest number is ", No1)
+    st.print("Greatest number is ", No1)
   elif (No2>No1 and No2>No3):
-    print("Greatest number is ", No2)
+    st.print("Greatest number is ", No2)
   elif (No3>No1 and No3>No2):
-    print("Greatest number is ", No3)
+    st.print("Greatest number is ", No3)
 
   data = {'Number1': No1,
           'Number2': No2,
